1e74b340a7e04f05c26b381d2c6e1a5fc5cf717e
Sajith Aravindan <sajitharavindan11@gmail.com>
2023-10-06 20:13:39 -0400

    Correction in server.js file

--- server.js
+++ server.js
@@ -1,16 +1,17 @@
+//Importing Dependencies
 const path = require('path');
 const express = require('express');
 const session = require('express-session');
 const exphbs = require('express-handlebars');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
-
+const sequelize = require('./config/connection');// import sequelize connection
 const routes = require('./controllers');
-const sequelize = require('./config/connection');
-const helpers = require('./utils/helpers');
+const helpers = require('./utils/helpers');// import helper functions
 
 const app = express();
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3001;//Define port
 
+//Declare session
 const sess = {
   secret: process.env.SS_DB_SECRET,
   cookie: {},
@@ -22,20 +23,22 @@ const sess = {
       expiration: 1000 * 60 * 30 // will expire after 30 minutes
   })
 };
-
 app.use(session(sess));
 
-const hbs = exphbs.create({ helpers });
+const hbs = exphbs.create({ helpers });//Declare Handlebars with Helper functions
 
+//Handlebars engines
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
 
+//Middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(routes);
 
+// sync sequelize models to the database, then turn on the server
 sequelize.sync({ force: false }).then(() => {
   app.listen(PORT, () => console.log(`Now listening at http://localhost:${PORT}/`));
 });
