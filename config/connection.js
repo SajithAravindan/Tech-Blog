ebb5cb3727bd3e140bcf15b6f39f1e65a38d2a49
Sajith Aravindan <sajitharavindan11@gmail.com>
2023-10-13 21:01:14 -0400

    corrected connect to jawsdb

--- config/connection.js
+++ config/connection.js
@@ -1,6 +1,8 @@
 const Sequelize = require('sequelize');// import sequelize
 require('dotenv').config();//import .env file
-const sequelize;
+
+let sequelize;
+
 if (process.env.JAWSDB_URL) {
   sequelize = new Sequelize(process.env.JAWSDB_URL);
 } else {
